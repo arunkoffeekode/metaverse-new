eea4d7ddef0ff8c697daf3b13a1cc8b204d18e8e
arunkoffeekode <arunkoffeekodes599@gmail.com>
2023-02-23 09:49:32 +0530

    23 Feb 2023 updated

--- src/Dashboard/DiscoverdWord.js
+++ src/Dashboard/DiscoverdWord.js
@@ -20,6 +20,43 @@ function DiscoverdWord() {
                             </div>
                         </div>
                     </div>
+                    <div className="row">
+                        <div className='col-lg-12 col-md-12 col-xl-12 col-xxl-12 col-sm-12'>
+                            <div className='Discoverd-title'>
+                                <h5>in to this world...</h5>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-sm-12 col-xl-4 col-xxl-4'>
+                            <div class="blog-item hover03">
+                                    <img src="images/world-1.png" alt="Blog" />
+                                <div class="blog-inner">
+                                    <h3>
+                                        The Room of True Love
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-sm-12 col-xl-4 col-xxl-4'>
+                            <div class="blog-item hover03">
+                                    <img src="images/world-2.png" alt="Blog" />
+                                <div class="blog-inner">
+                                    <h3>
+                                      This Spaceship Hangar
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='col-lg-4 col-md-6 col-sm-12 col-xl-4 col-xxl-4'>
+                            <div class="blog-item hover03">
+                                    <img src="images/world-3.png" alt="Blog" />
+                                <div class="blog-inner">
+                                    <h3>
+                                        Immersive Space Adventure
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
