f28e831c1fbf2882faeef508d57bf966a6480f01
arunkoffeekode <arunkoffeekodes599@gmail.com>
2023-03-10 18:32:21 +0530

    10 March 2023 6:30 uploaded

--- src/Dashboard/About.js
+++ src/Dashboard/About.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import avt from '../img/avt.png'
 function About() {
     return (
         <div>
@@ -7,6 +7,19 @@ function About() {
                 <div class="content container-fluid">
                     <div className='row'>
                         <div className='col-lg-6 col-md-12 col-xl-6 col-xxl-6 col-sm-12'>
+                        </div>
+                        <div className='col-lg-6 col-md-12 col-xl-6 col-xxl-6 col-sm-12'>
+                            <div className='about-avt'>
+                                <div className='avt-img'>
+                                    <img src={avt} alt="" />
+                                </div>
+                                <div className='avt-details'>
+                                    <h3>BP8789</h3>
+                                    <p>Surat, Gujarat (India) </p>
+                                    <span className='dot'>Online</span>
+                                    <h5>Looking For <span>(Male, 20 - 38)</span></h5>
+                                </div>
+                            </div>
                             <div className='about-section'>
                                 <div className='about-details'>
                                     <div className='about-title'>
@@ -26,30 +39,92 @@ function About() {
                                         </div>
                                     </div>
                                 </div>
-                                
-                                <div className='about-details'>
+                                <div className='about-details mb-0'>
                                     <div className='about-title-2 p-0'>
-                                        <ul class="nav nav-pills" id="pills-tab" role="tablist">
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Basic</button>
+                                        <ul className="nav nav-pills" id="pills-tab" role="tablist">
+                                            <li className="nav-item" role="presentation">
+                                                <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Basic</button>
                                             </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">About me </button>
+                                            <li className="nav-item" role="presentation">
+                                                <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">About me </button>
                                             </li>
-                                            <li class="nav-item" role="presentation">
-                                                <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">hobbies & interest</button>
+                                            <li className="nav-item" role="presentation">
+                                                <button className="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">hobbies & interest</button>
                                             </li>
                                         </ul>
                                     </div>
                                     <div className='about-box'>
-                                        <div class="tab-content" id="pills-tabContent">
-                                            <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">..1.</div>
-                                            <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">..2.</div>
-                                            <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">.3..</div>
+                                        <div className="tab-content" id="pills-tabContent">
+                                            <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
+                                                <table>
+                                                    <tr>
+                                                        <td>Gender :</td>
+                                                        <td>Female</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>age :</td>
+                                                        <td>28</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Height :</td>
+                                                        <td>163 cm</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Weight :</td>
+                                                        <td>68 kg</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Looking For :</td>
+                                                        <td>Male, 20 - 38</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                            <div className="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">
+                                                <table>
+                                                    <tr>
+                                                        <td>Lives in :</td>
+                                                        <td>Surat, Gujarat (India)</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nationality :</td>
+                                                        <td>Indian</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Languages spoken :</td>
+                                                        <td>English, Hindi, Gujarati</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Marital Status :</td>
+                                                        <td>Single</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Occupation :</td>
+                                                        <td>Developer</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                            <div className="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">
+                                                <div className='hobies'>
+                                                    <ul>
+                                                        <li>
+                                                            <button className='hobi-btn' type='button'>Arts & Crafts</button>
+                                                        </li>
+                                                        <li>
+                                                            <button className='hobi-btn' type='button'>Entertainment & Fun</button>
+                                                        </li>
+                                                        <li>
+                                                            <button className='hobi-btn' type='button'>Sports</button>
+                                                        </li>
+                                                        <li>
+                                                            <button className='hobi-btn' type='button'>Outdoor Activities</button>
+                                                        </li>
+                                                    </ul>
+
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
